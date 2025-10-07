8499dd4ff9376d1832e1222ea6571cf9ebd76dd0
hehe <141596927+ernvsy@users.noreply.github.com>
2025-10-07 19:02:14 +0700

    Add files via upload

---
+++ test-1/19A_bungagabungan.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    int p,q;
+    int i,k,t;
+    int jumlah;
+    cin>>p>>q;
+    i= p*p;
+    q= k*k;
+    t= 1;
+    jumlah= i + q + t;
+    if( jumlah % 4== 0){
+        cout<< jumlah/4;
+    }else {
+        cout<< -1;
+    }
+    
+}
